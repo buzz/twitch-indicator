4ee89469870a33ca77dec4873dd18855e031b527
buzz <buzz@users.noreply.github.com>
2022-07-16 17:33:46 +0200

    libnotify update to 0.8

--- twitch_indicator/__main__.py
+++ twitch_indicator/__main__.py
@@ -3,7 +3,7 @@
 import gi
 
 gi.require_version("AppIndicator3", "0.1")
-gi.require_version("Notify", "0.7")
+gi.require_version("Notify", "0.8")
 gi.require_version("Gtk", "3.0")
 
 from twitch_indicator.app import TwitchIndicatorApp
