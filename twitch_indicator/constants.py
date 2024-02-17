7327a5e04aaf7a0ba2e0dcab0e8a4a6fb314a818
buzz <buzz@users.noreply.github.com>
2024-02-17 02:19:29 +0100

    v1.7

--- twitch_indicator/constants.py
+++ twitch_indicator/constants.py
@@ -3,7 +3,7 @@ import os.path
 from gi.repository import GLib
 
 APP_NAME = "Twitch Indicator"
-VERSION = "1.6"
+VERSION = "1.7"
 
 TWITCH_WEB_URL = "https://www.twitch.tv/"
 TWITCH_API_URL = "https://api.twitch.tv/helix/"
